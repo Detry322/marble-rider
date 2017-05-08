ef368ee488ea92d599e30d60b830d1531560a8e9
Jack Serrino <Jserrino@gmail.com>
2017-05-08 18:04:23 -0400

    Adjustable sphere

--- src/brushes/marbleBrush.js
+++ src/brushes/marbleBrush.js
@@ -4,25 +4,26 @@ AFRAME.registerBrush('marbleBrush',
     init: function (color, width) {
       this.material = new THREE.MeshStandardMaterial({
         color: this.data.color,
-        roughness: 0.5,
-        metalness: 0.5,
-        side: THREE.DoubleSide,
-        shading: THREE.FlatShading
+        roughness: 0.6,
+        metalness: 0.2,
+        side: THREE.FrontSide,
+        shading: THREE.SmoothShading
       });
-      this.geometry = new THREE.SphereGeometry(2);
+      this.geometry = new THREE.IcosahedronGeometry(1, 2);
+      this.mesh = new THREE.Mesh(this.geometry, this.material);
+      this.object3D.add(this.mesh);
+      this.mesh.visible = false
     },
     addPoint: function (position, orientation, pointerPosition, pressure, timestamp) {
-      var box = new THREE.Mesh(this.geometry, this.material);
-
-      var sca = this.data.size;
-      box.scale.set(sca, sca, sca);
-      box.position.copy(pointerPosition);
-      box.rotation.copy(orientation);
-
-      this.object3D.add(box);
-
+      if (!this.firstPoint) {
+        this.firstPoint = pointerPosition.clone();
+        this.mesh.position.set(this.firstPoint.x, this.firstPoint.y, this.firstPoint.z)
+      }
+      this.mesh.visible = true
+      var distance = this.firstPoint.distanceTo(pointerPosition);
+      this.mesh.scale.set(distance, distance, distance);
       return true;
     }
   },
-  {thumbnail: 'brushes/thumb_cubes.gif', spacing: 0.01}
+  {thumbnail: 'brushes/thumb_single_sphere.png', spacing: 0.0}
 );
