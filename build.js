5346ddb9053e8fb64ea7dc62b768dd629c9d0272
Jack Serrino <Jserrino@gmail.com>
2017-05-14 22:24:11 -0400

    Make progress

--- build.js
+++ build.js
@@ -3656,11 +3656,13 @@
 	    finishStroke: function() {
 	      this.entity.setAttribute('position', {x: this.mesh.position.x, y: this.mesh.position.y, z: this.mesh.position.z});
 	      this.mesh.position.set(0, 0, 0);
-	      this.entity.setAttribute('rotation', {
+	      rotation = {
 	        x: THREE.Math.radToDeg(this.mesh.rotation.x),
 	        y: THREE.Math.radToDeg(this.mesh.rotation.y),
 	        z: THREE.Math.radToDeg(this.mesh.rotation.z)
-	      });
+	      }
+	      this.entity.setAttribute('rotation', rotation);
+	      console.log(rotation);
 	      this.mesh.rotation.set(0, 0, 0);
 	      this.entity.setAttribute('static-body', '');
 	    }
